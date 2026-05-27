2f262e2566bd04967e28c025c2ec2d3e987116d5
AnneCin <anne.hinken@gmail.com>
2026-05-27 11:50:14 +0200

    Add lm() function and summary statistics (LSE section)

--- 07-linear-regression/section2_linear_models/galton_linear_models.R
+++ 07-linear-regression/section2_linear_models/galton_linear_models.R
@@ -45,4 +45,12 @@ results %>%
        x = "beta1", y = "RSS")
 
 # but this is not necesary true for all beta0.
+#Solution? lm function -->
+
+# fit regression line to predict son's height from father's height
+fit <- lm(son ~ father, data = galton_heights)
+fit
+
+# summary statistics
+summary(fit)
 
