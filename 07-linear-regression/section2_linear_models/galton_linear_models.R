60aa2d06c96fde749f55ccc44e3f84f72f4f7a01
AnneCin <anne.hinken@gmail.com>
2026-06-01 10:53:27 +0200

    Add advanced LSE note: correlation between beta_0 and beta_1, effect of centering

--- 07-linear-regression/section2_linear_models/galton_linear_models.R
+++ 07-linear-regression/section2_linear_models/galton_linear_models.R
@@ -1,18 +1,13 @@
-
 library(tidyverse)
 library(HistData)
 library(ggplot2)
 
 # 1. DATA PREPARATION ------------------------------------------
-
 data("GaltonFamilies")
-
 # Select one son per family to ensure independence
 set.seed(1983) 
 # Fixes the random generator so that the sample is always identical.
 # Important for reproducibility!
-
-# compute RSS for any pair of beta0 and beta1 in Galton's data
 galton_heights <- GaltonFamilies %>%
   filter(gender == "male") %>%
   group_by(family) %>%
@@ -21,7 +16,7 @@ galton_heights <- GaltonFamilies %>%
   select(father, childHeight) %>%
   rename(son = childHeight)
 
-# 1. LEAST SQUARES ESTIMATES -----------------------------------
+# 2. LEAST SQUARES ESTIMATES -----------------------------------
 # RSS function: calculates the sum of squared residuals
 rss <- function(beta0, beta1, x, y){
   resid <- y - (beta0 + beta1*x)
@@ -30,41 +25,31 @@ rss <- function(beta0, beta1, x, y){
 
 # Plot RSS as a function of beta1 when beta0 = 25
 beta1 <- seq(0, 1, len = nrow(galton_heights))
-
 results <- data.frame(
   beta1 = beta1,
   rss = sapply(beta1, rss, beta0 = 25, 
                x = galton_heights$father, 
                y = galton_heights$son)
 )
-
 results %>% 
   ggplot(aes(beta1, rss)) + 
   geom_line() +
   labs(title = "RSS as a function of beta1 (beta0 fixed at 25)",
        x = "beta1", y = "RSS")
+# but this is not necessarily true for all beta0.
 
-# but this is not necesary true for all beta0.
-#Solution? lm function -->
-
-# fit regression line to predict son's height from father's height
-
-#When calling the lm() function, the variable that we want to predict is put to the left of the ~ symbol, 
-#and the variables that we use to predict is put to the right of the ~ symbol. 
-#The intercept is added automatically.'
-
+# Solution: lm function
+# The variable to predict goes left of ~, predictors go right.
+# The intercept is added automatically.
 fit <- lm(son ~ father, data = galton_heights)
 fit
-
-# summary statistics
 summary(fit)
 
-#Add Monte Carlo simulation for LSE as random variables------
+# 3. MONTE CARLO SIMULATION: LSE ARE RANDOM VARIABLES ----------
+B <- 1000
+N <- 50
 
-B<-1000
-N<-50
 # .$coef extracts only the coefficients table from the summary output
-# (Estimate, Std. Error, t value, p-value)
 lse <- replicate(B, {
   sample_n(galton_heights, N, replace = TRUE) %>% 
     lm(son ~ father, data = .) %>% 
@@ -80,15 +65,27 @@ p1 <- lse %>% ggplot(aes(beta_0)) + geom_histogram(binwidth = 5, color = "black"
 p2 <- lse %>% ggplot(aes(beta_1)) + geom_histogram(binwidth = 0.1, color = "black") 
 grid.arrange(p1, p2, ncol = 2)
 
-# summary statistics
+# Compare: standard error from a single sample vs. 1000 Monte Carlo simulations
+# Both should be similar - confirms that lm() accurately estimates the standard error
 sample_n(galton_heights, N, replace = TRUE) %>% 
   lm(son ~ father, data = .) %>% 
   summary %>%
   .$coef
 
-# Compare: standard error from a single sample (lm summary) vs. 
-# standard error estimated from 1000 Monte Carlo simulations (sd of beta_0 and beta_1)
-# Both should be similar - confirms that lm() accurately estimates the standard error
-
 lse %>% summarize(se_0 = sd(beta_0), se_1 = sd(beta_1))
 
+# 4. ADVANCED NOTE: CORRELATION BETWEEN LSE --------------------
+# beta_0 and beta_1 are strongly negatively correlated (~-1)
+# because if slope increases, intercept must decrease to fit the data
+lse %>% summarize(cor(beta_0, beta_1))
+
+# Centering the predictor reduces this correlation
+# When father is centered (father - mean(father)), beta_0 becomes
+# the predicted son height for an average father -> more interpretable
+lse_centered <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    mutate(father = father - mean(father)) %>%
+    lm(son ~ father, data = .) %>% 
+    .$coef 
+})
+cor(lse_centered[1,], lse_centered[2,])
